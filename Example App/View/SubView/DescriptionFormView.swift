25bf2b31bbb221570b96d285fb9999c05f5972bf
Emmanuel Ramirez <emmanuel.ramirez@sicar.mx>
2025-01-14 14:38:21 -0600

    first commit

--- Example App/View/SubView/DescriptionFormView.swift
+++ Example App/View/SubView/DescriptionFormView.swift
@@ -60,38 +60,3 @@ struct DescriptionFormView: View {
     }
     
 }
-
-class DescriptionFormViewModel: ObservableObject {
-    
-    private var cancellable: AnyCancellable?
-    @Published var invalidMessage: String? = nil
-    @Published var description: String = ""
-    
-    init() {
-        cancellable = $description
-            .debounce(for: 0.450, scheduler: RunLoop.main)
-            .sink { [weak self] newValue in
-                self?.validation()
-            }
-    }
-    
-    private func validation() {
-        if validateDescription() {
-            invalidMessage = nil
-        } else {
-            invalidMessage = "Characters invalid"
-        }
-    }
-    
-    func validateDescription() -> Bool {
-        
-        guard !description.isEmpty else { return true }
-        
-        if description.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
-            return false
-        }
-        
-        return true
-    }
-    
-}
--- Example App/ViewModel/DescriptionFormViewModel.swift
+++ Example App/ViewModel/DescriptionFormViewModel.swift
@@ -1,7 +1,44 @@
 //
-//  Untitled.swift
+//  DescriptionFormViewModel.swift
 //  Example App
 //
 //  Created by Emmanuel Ramírez on 14/01/25.
 //
 
+import Combine
+import Foundation
+
+class DescriptionFormViewModel: ObservableObject {
+    
+    private var cancellable: AnyCancellable?
+    @Published var invalidMessage: String? = nil
+    @Published var description: String = ""
+    
+    init() {
+        cancellable = $description
+            .debounce(for: 0.450, scheduler: RunLoop.main)
+            .sink { [weak self] newValue in
+                self?.validation()
+            }
+    }
+    
+    private func validation() {
+        if validateDescription() {
+            invalidMessage = nil
+        } else {
+            invalidMessage = "Characters invalid"
+        }
+    }
+    
+    func validateDescription() -> Bool {
+        
+        guard !description.isEmpty else { return true }
+        
+        if description.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            return false
+        }
+        
+        return true
+    }
+    
+}
